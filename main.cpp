0c28f723c3829158111714261a073a0724cb78dd
StarmarkLN <76650983+StarmarkLN@users.noreply.github.com>
2023-07-05 17:16:13 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,15 +11,15 @@
 
 int main()
 {
-    if (__cplusplus == 201703L) std::cout << "C++17\n";		//--- для параметра -std=c++17	
+    if (__cplusplus == 201703L) std::cout << "C++17\n";	    //--- для параметра -std=c++17	
     else if (__cplusplus == 201402L) std::cout << "C++14\n";//--- для параметра -std=c++14
     else if (__cplusplus == 201103L) std::cout << "C++11\n";//--- для параметра -std=c++15
     else if (__cplusplus == 199711L) std::cout << "C++98\n";//--- для параметра -std=c++98
-    else std::cout << "pre-standard C++\n"; 				//--- для параметра -std=c++2a
+    else std::cout << "pre-standard C++\n"; 		    //--- для параметра -std=c++2a
 
     //--- лямбда принимает пару строк по ссылкам const std::string& 
-	//--- и выводит пару const std::string 
-	//--- благодаря типу auto возвращаемого результата
+    //--- и выводит пару const std::string 
+    //--- благодаря типу auto возвращаемого результата
     auto func = [](const std::string& first, const std::string& second)
     {
         std::cout << first << second;
